85b7f8d9ee002a4490bfe49e608bdf1f9b59bb9b
liuleiy <1655425171@qq.com>
2018-10-30 10:16:33 +0800

    1

--- src/main/java/com/simulation/admin/model/TUser.java
+++ src/main/java/com/simulation/admin/model/TUser.java
@@ -1,10 +1,12 @@
 package com.simulation.admin.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "t_user")
 public class TUser {
-
+	@Id
+	private Integer id;
 }
